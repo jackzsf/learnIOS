cb98153c9b5327af6ef409fdedbb45e0c54ce31f
zhangsaifeng <zhangsaifeng1218@163.com>
2020-05-25 14:55:40 +0800

    add LRUCacheMap algrorithm

--- learnIOS/ViewController.m
+++ learnIOS/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "LRUCacheMap.h"
 
 @interface ViewController ()
+
+@property(nonatomic, strong) LRUCacheMap *lruCacheMap;
 
 @end
 
@@ -17,6 +20,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self lRUCacheMapTest];
+}
+
+- (void)lRUCacheMapTest{
+    NSLog(@"lRUCacheMapTest...");
+    for (int i = 1; i <=5; i++){
+        NSString *key = [NSString stringWithFormat:@"%d",i];
+        NSString *value = [NSString stringWithFormat:@"%d",i*100];
+        
+        [self.lruCacheMap setValue:value forKey:key];
+    }
+    
+    [self.lruCacheMap enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        NSLog(@"key ,value == %@, %@",key,obj);
+    }];
+    
+    NSLog(@"-------------");
+    
+    [self.lruCacheMap setValue:@"600" forKey:@"6"];
+    
+    [self.lruCacheMap enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        NSLog(@"key ,value == %@, %@",key,obj);
+    }];
+    
+    NSLog(@"-------------");
+    
+    NSLog(@"%@",[self.lruCacheMap valueForKey:@"5"]);
+    
+    [self.lruCacheMap enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        NSLog(@"key ,value == %@, %@",key,obj);
+    }];
+    
+    NSLog(@"-------------");
+    
+    NSLog(@"%@",[self.lruCacheMap valueForKey:@"4"]);
+    
+    [self.lruCacheMap enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        NSLog(@"key ,value == %@, %@",key,obj);
+    }];
+}
+
+- (LRUCacheMap *)lruCacheMap{
+    if (_lruCacheMap == nil) {
+        _lruCacheMap = [[LRUCacheMap alloc] initWithCapacity:5];
+    }
+    
+    return _lruCacheMap;
 }
 
 
